203d0cb94388923ec208d22b8157930f9c00b2c9
Natalia Vera <nvera@coopebombas.com>
2022-08-05 07:32:20 -0500

    Initial Commit

---
+++ ToDo-App/ToDo_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo_AppApp.swift
+//  ToDo-App
+//
+//  Created by Desarrollador1 on 5/08/22.
+//
+
+import SwiftUI
+
+@main
+struct ToDo_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
